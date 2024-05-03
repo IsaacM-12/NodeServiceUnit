3f0b00e69159b885e9f0855e49d74c486315253c
IsaacM-12 <isaacgatgens13@gmail.com>
2024-05-03 13:45:42 -0600

    siu

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo desde Express!');
+  res.send('¡Hola, mundo desde Express SIUUU!');
 });
 
 app.listen(PORT, () => {
