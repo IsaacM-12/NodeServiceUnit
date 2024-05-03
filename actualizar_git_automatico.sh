79601a17f12783875a6a9b89bf4848aa7c929182
IsaacM-12 <isaacgatgens13@gmail.com>
2024-05-03 14:54:53 -0600

    intentando actualizar desde deployment

---
+++ actualizar_git_automatico.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Ruta del directorio de tu aplicación Node.js
+APP_DIRECTORY="/home/isaacmelendez/helloNode"
+
+# Rama del repositorio que deseas verificar
+BRANCH="Deployment"
+
+# Ruta del repositorio en GitHub
+GITHUB_REPO="https://github.com/IsaacM-12/NodeServiceUnit.git"
+
+# Cambia al directorio de la aplicación
+cd "$APP_DIRECTORY"
+
+# Comprobar si hay cambios en la rama específica del repositorio remoto
+if git fetch origin "$BRANCH" && [ "$(git rev-list HEAD...origin/"$BRANCH" --count)" -gt 0 ]; then
+    echo "Se han detectado cambios en la rama $BRANCH."
+
+    # Actualizar la rama local con los cambios remotos
+    git pull origin "$BRANCH"
+
+    # Reiniciar la aplicación Node.js
+    pm2 restart <nombre_del_proceso_o_script>
+
+    echo "La aplicación ha sido actualizada correctamente."
+else
+    echo "No se han detectado cambios en la rama $BRANCH."
+fi
