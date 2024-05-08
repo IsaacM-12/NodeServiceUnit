f56358b999a5fbc5c52ab93a66ff9babbcb35f6a
IsaacM-12 <85079662+IsaacM-12@users.noreply.github.com>
2024-05-08 13:37:08 -0600

    Update node.js

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo desde Express!');
+  res.send('¡push nuevo!');
 });
 
 app.listen(PORT, () => {
